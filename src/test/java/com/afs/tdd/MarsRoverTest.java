9bca7052bd48d912740f6f4bc76421b24f85b53f
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-17 21:27:45 +0800

    test: added test definition to should_change_to_location_0_0_North_when_executeCommand_given_0_0_West_and_command_Turn_Right

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -169,5 +169,17 @@ class MarsRoverTest {
     }
 
     @Test
-    void should_change_to_location_0_0_North_when_executeCommand_given_0_0_West_and_command_Turn_Right(){}
+    void should_change_to_location_0_0_North_when_executeCommand_given_0_0_West_and_command_Turn_Right(){
+        //Given
+        Location initialLocation = new Location(0, 0, Direction.WEST);
+        Command givenCommand = Command.TURN_RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        //When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        //Then
+        Assertions.assertEquals(0,currentLocation.getX());
+        Assertions.assertEquals(0,currentLocation.getY());
+        Assertions.assertEquals(Direction.NORTH,currentLocation.getDirection());
+    }
 }
