aed8999322ddb991b9a3a5ddeca34af969a4a7ad
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-17 20:41:52 +0800

    feat: added handling to test should_change_to_location_0_0_East_when_executeCommand_given_0_0_South_and_command_Turn_Left

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -21,6 +21,9 @@ public class MarsRover {
             if(location.getDirection() == Direction.NORTH){
                 location.setDirection(Direction.WEST);
             }
+            else if(location.getDirection() == Direction.SOUTH){
+                location.setDirection(Direction.EAST);
+            }
         }
         else if(givenCommand == Command.TURN_RIGHT){
             if(location.getDirection() == Direction.NORTH){
